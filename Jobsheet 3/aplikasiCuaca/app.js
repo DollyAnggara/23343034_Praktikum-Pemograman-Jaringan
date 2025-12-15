b66b0764abf83b3dca02619725def9c106e7f890
Dolly Anggara <145577242+DollyAnggara@users.noreply.github.com>
2025-12-15 21:58:33 +0700

    Delete Jobsheet 3 directory

--- Jobsheet 3/aplikasiCuaca/app.js
+++
@@ -1,9 +0,0 @@
-const request = require('postman-request')
-const url = 'http://api.weatherstack.com/current?access_key=f967c5f0f4761a19a1bff0caeddc3a85&query=-0.8980323611374166,100.35051338911944'
-request({ url: url }, (error, response) => {
-  //console.log(response)
-  const data = JSON.parse(response.body)
-  //console.log(data)
-  //console.log(data.current)
-  console.log(data.current.temperature)
-})
